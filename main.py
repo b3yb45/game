5d01d68aa1da0f3d1dfe89c026ef2b2f4ad6c7ce
b3yb45 <126542355+b3yb45@users.noreply.github.com>
2023-05-16 00:52:48 +0700

    fix3

--- main.py
+++ main.py
@@ -647,6 +647,8 @@ mars_var = moon_var
 venus_var = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(Ru_V.greeting)
+print('')
+
 venus = input('Имена исследователей Венеры: ')
 print(venus+','+' добро пожаловать на исследовательскую станцию Венеры!')
 
